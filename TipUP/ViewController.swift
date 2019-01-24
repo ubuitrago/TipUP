54a3bdb5c1735af4dc844d31b07ba17aff2464cb
Buitrago, Uriel <uab62@lib-pclem021.local>
2019-01-24 16:44:00 -0600

    Initial Commit

---
+++ TipUP/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TipUP
+//
+//  Created by Buitrago, Uriel on 1/24/19.
+//  Copyright © 2019 Buitrago, Uriel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
