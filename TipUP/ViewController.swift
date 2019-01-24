5648a86d1eff623a2459e6d8ffae532f45796e83
Buitrago, Uriel <uab62@lib-pclem021.lib.utexas.edu>
2019-01-24 17:16:53 -0600

    Tip Up first revision

--- TipUP/ViewController.swift
+++ TipUP/ViewController.swift
@@ -14,7 +14,30 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    //outlets
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    //
+    
+    @IBAction func onTap(_ sender: Any) {
+        view.endEditing(true)
+    }
+    @IBAction func calculateTip(_ sender: Any) {
+        // Get the bill amount
+        let bill = Double(billField.text!)  ?? 0
+        
+        // Calculate the tip and total
+        let tipPer = [0.15, 0.18, 0.2]
+        let tip = bill * tipPer[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        // Update the tip and total labels
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
