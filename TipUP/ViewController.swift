ba02126f56a91c95141e73d731e62a9c16cc2c95
Buitrago, Uriel <uab62@lib-pclem023.lib.utexas.edu>
2019-01-25 21:58:29 -0600

    Settings screen added

--- TipUP/ViewController.swift
+++ TipUP/ViewController.swift
@@ -12,7 +12,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // sets the title of the navigation bar
+        self.title = "TipUP!"
     }
     
     //outlets
